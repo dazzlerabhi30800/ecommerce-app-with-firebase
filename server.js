648ea8fb5f0b933545019db94c7edc4c571bc2f4
= <abhisingh30800@gmail.com>
2023-07-19 19:04:08 +0530

    added live key

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ config();
 
 // Require the framework and instantiate it
 const fastifyp = fastify({ logger: true });
+// const secretKey =
+//   "sk_test_51NVU2CSCjGfm4lCe6K3bspHaYCsd0GEP6EfRUffxq631m3GhqhXGpAvBBxaJmUKl6m1rHH5Fh1VoaKw6useW6Nfz00KnA5ZhbH";
 const secretKey =
-  "sk_test_51NVU2CSCjGfm4lCe6K3bspHaYCsd0GEP6EfRUffxq631m3GhqhXGpAvBBxaJmUKl6m1rHH5Fh1VoaKw6useW6Nfz00KnA5ZhbH";
+  "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";
+const publishKey =
+  "pk_live_51NVU2CSCjGfm4lCe1jozKlDObpABqt0jJ0kxH3dkVmtNPkpS0vrESG4hPmJnQL2XQfWtJj0QTOB9xehIKfUsCJYv00riAnwlTD";
 const stripe = Stripe(secretKey);
 
 fastifyp.get("/publishable-key", () => {
   return {
     publishable_key:
-      "pk_test_51NVU2CSCjGfm4lCeNIhRcYbt81ohE3vFdLvGzDQ958j5gkG2amqWZyavvxx5r4af2qgAyB6J33iypbPLrv4vLQfE00PBMfcYbO",
+      // "pk_test_51NVU2CSCjGfm4lCeNIhRcYbt81ohE3vFdLvGzDQ958j5gkG2amqWZyavvxx5r4af2qgAyB6J33iypbPLrv4vLQfE00PBMfcYbO",
+      publishKey,
   };
 });
 
